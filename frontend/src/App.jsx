f287e91dd79a2d51f7c0db1a8068232a5b2c6674
elmi08 <elmimoha08@gmail.com>
2024-05-22 23:23:57 +0300

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import Home from './pages/Home/Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import SignUp from "./pages/SignUp/SignUp"
+import Login from "./pages/Login/Login"
+
+
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />} />
+      <Route path='/signUp' exact element={<SignUp />} />
+      <Route path='/login' exact element={<Login />} />
+    </Routes>
+  </Router>
+)
+
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+  return <div>{routes}</div>
+
 }
 
 export default App
