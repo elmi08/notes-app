312eb8a0aaa0908af6e53f2365c20c899add4015
elmi08 <elmi08@gmail.com>
2024-06-18 19:42:11 +0300

    updated yaml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
- base: "/",
- plugins: [react()],
- preview: {
-  port: 3000,
-  strictPort: true,
- },
- server: {
-  port: 3000,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:8080",
- },
+  plugins: [react()],
+  build: {
+    outDir: 'frontend/dist',
+    assetsDir: '', 
+  },
 });
