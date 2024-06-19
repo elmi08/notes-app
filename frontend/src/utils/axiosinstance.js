f217cd3a793757d2f17486f1afa77017921aa9ba
elmi08 <elmi08@gmail.com>
2024-06-19 22:16:13 +0300

    update env

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_SERVER_URL,
     headers: {
         'Content-Type': 'application/json',
+        'BaseUrl': import.meta.env.VITE_BACKEND_SERVER_URL
     },
 })
 
